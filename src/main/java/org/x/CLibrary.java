6fef5b5d2cd039c1fa87aa6c034669c83ef92f45
brett-smith <brett@hypersocket.com>
2018-08-15 22:10:07 +0100

    Fixed license headers.

--- src/main/java/org/x/CLibrary.java
+++ src/main/java/org/x/CLibrary.java
@@ -1,3 +1,35 @@
+/**
+ * XDAMAGE Java Bindings - This is a simple extension of JNA Platform, adding the XFixes and XDamage extensions (and a couple more X11 functions)
+ * Copyright © ${project.inceptionYear} SSHTOOLS Limited (support@sshtools.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * SSHTOOLS Limited licenses this file to you under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this 
+ * file except in compliance with the License.  
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.x;
 
 import java.util.List;
@@ -8,14 +40,14 @@ import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 
 public interface CLibrary extends Library {
-
-	public final static int IPC_CREAT = 00001000; /* create if key is nonexistent */
+	public final static int IPC_CREAT = 00001000; /*
+													 * create if key is
+													 * nonexistent
+													 */
 	public final static int IPC_EXCL = 00002000; /* fail if key exists */
 	public final static int IPC_NOWAIT = 00004000; /* return error on wait */
 	public static final int IPC_PRIVATE = 0; /* ((__kernel_key_t) 0) ?? */
-
 	public static final int IPC_RMID = 0; /* remove resource */
-
 	CLibrary INSTANCE = (CLibrary) Native.loadLibrary("c", CLibrary.class);
 
 	Pointer shmat(int shmid, final Pointer shmaddr, int shmflg);
@@ -23,17 +55,15 @@ public interface CLibrary extends Library {
 	int shmget(int key, int size, int shmflg);
 
 	int shmdt(Pointer shmaddr);
-	
+
 	static class shmid_ds extends Structure {
 		// TODO - not used yet
 		@Override
 		protected List<String> getFieldOrder() {
-//			return Arrays.asList("shm_perm", );
+			// return Arrays.asList("shm_perm", );
 			return null;
 		}
-		
 	}
 
 	void shmctl(int shmid, int ipcRmid, shmid_ds buf);
-
 }
--- src/main/java/org/x/ExtUser32.java
+++ src/main/java/org/x/ExtUser32.java
@@ -1,3 +1,20 @@
+/**
+ * XDAMAGE Java Bindings - This is a simple extension of JNA Platform, adding the XFixes and XDamage extensions (and a couple more X11 functions)
+ * Copyright © ${project.inceptionYear} SSHTOOLS Limited (support@sshtools.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.x;
 
 import java.util.Arrays;
@@ -12,59 +29,55 @@ import com.sun.jna.platform.win32.User32;
 import com.sun.jna.win32.W32APIOptions;
 
 public interface ExtUser32 extends User32 {
-
 	/** The instance. */
-	ExtUser32 INSTANCE = (ExtUser32) Native.loadLibrary("user32", ExtUser32.class,
-			W32APIOptions.DEFAULT_OPTIONS);
+	ExtUser32 INSTANCE = (ExtUser32) Native.loadLibrary("user32", ExtUser32.class, W32APIOptions.DEFAULT_OPTIONS);
 
 	/**
-	 * The GetCursorPos function retrieves the position of the mouse cursor, in screen coordinates.
+	 * The GetCursorPos function retrieves the position of the mouse cursor, in
+	 * screen coordinates.
 	 * 
-	 * @param lpPoint
-	 *            Pointer to a LPPOINT structure to receive the information.
+	 * @param lpPoint Pointer to a LPPOINT structure to receive the information.
 	 * @return If the function succeeds, the return value is nonzero. If the
 	 *         function fails, the return value is zero.
 	 */
 	boolean GetCursorPos(POINT lpPoint);
 
-    public static class CWPSTRUCT extends Structure {
-        public LPARAM lParam;
-        public WPARAM wParam;
-        public int message;
-        public HWND hwnd;
-        
+	public static class CWPSTRUCT extends Structure {
+		public LPARAM lParam;
+		public WPARAM wParam;
+		public int message;
+		public HWND hwnd;
+
 		protected List<?> getFieldOrder() {
 			return Arrays.asList("lParam", "wParam", "message");
 		}
-    }
-    
-    public final static int HC_ACTION = 0;
-    public final static int HC_NOREMOVE = 3;
-    
-    public final static int WH_CALLWNDPROC = 4;
-    public final static int WH_GETMESSAGE = 3;
-    public final static int WH_CALLWNDPROCRET = 12;
-    public final static int WH_CBT = 5;
-    public final static int WH_DEBUG = 9;
-    public final static int WH_SYSMSGFILTER = 6;
+	}
 
-    public final static int WM_CREATE = 0x0001;
-    public final static int WM_DESTROY = 0x0002;
-    public final static int WM_WINDOWPOSCHANGING = 0x0046;
-    
-    interface EnumWindowsProc {
-    	boolean callback(HWND hwnd, CWPSTRUCT lParam);
-    }
+	public final static int HC_ACTION = 0;
+	public final static int HC_NOREMOVE = 3;
+	public final static int WH_CALLWNDPROC = 4;
+	public final static int WH_GETMESSAGE = 3;
+	public final static int WH_CALLWNDPROCRET = 12;
+	public final static int WH_CBT = 5;
+	public final static int WH_DEBUG = 9;
+	public final static int WH_SYSMSGFILTER = 6;
+	public final static int WM_CREATE = 0x0001;
+	public final static int WM_DESTROY = 0x0002;
+	public final static int WM_WINDOWPOSCHANGING = 0x0046;
 
-    interface CallWndProc extends HOOKPROC {
-        LRESULT callback(int nCode, WPARAM wParam, CWPSTRUCT lParam);
-    }
+	interface EnumWindowsProc {
+		boolean callback(HWND hwnd, CWPSTRUCT lParam);
+	}
 
-    interface GetMsgProc extends HOOKPROC {
-        LRESULT callback(int nCode, WPARAM wParam, CWPSTRUCT lParam);
-    }
+	interface CallWndProc extends HOOKPROC {
+		LRESULT callback(int nCode, WPARAM wParam, CWPSTRUCT lParam);
+	}
 
-    interface SysMsgProc extends HOOKPROC {
-        LRESULT callback(int nCode, WPARAM wParam, CWPSTRUCT lParam);
-    }
+	interface GetMsgProc extends HOOKPROC {
+		LRESULT callback(int nCode, WPARAM wParam, CWPSTRUCT lParam);
+	}
+
+	interface SysMsgProc extends HOOKPROC {
+		LRESULT callback(int nCode, WPARAM wParam, CWPSTRUCT lParam);
+	}
 }
