701199ca45fb5994d778402ec497dd58b732fed2
brett-smith <brett@hypersocket.com>
2019-09-26 09:07:49 +0100

    Seems was down to license formatting

--- src/main/java/org/x/X11.java
+++ src/main/java/org/x/X11.java
@@ -1,3 +1,21 @@
+/**
+ * XDAMAGE Java Bindings - This is a simple extension of JNA Platform, adding the XFixes and XDamage extensions (and a couple more X11 functions)
+ * Copyright © 2012 SSHTOOLS Limited (support@sshtools.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* Copyright (c) 2007 Timothy Wall, All Rights Reserved
  *
  * The contents of this file is dual-licensed under 2
